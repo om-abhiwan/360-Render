2d77ec3b7911918e3aacdffcf3f1c5eea5bf3821
om-abhiwan <om.abhiwan@gmail.com>
2024-05-02 19:14:14 +0530

    updated rotation but not done

--- src/pages/Render.jsx
+++ src/pages/Render.jsx
@@ -30,42 +30,34 @@ const Render = () => {
     const createScene = (engine, canvas) => {
         const scene = new BABYLON.Scene(engine);
 
-        var camera = new BABYLON.ArcRotateCamera("Camera", -(Math.PI / 2) + 2, Math.PI / 2, 5, BABYLON.Vector3.Zero(), scene);
+        const camera = new BABYLON.ArcRotateCamera(
+            // "Camera", -(Math.PI / 2) + 2, Math.PI / 2, 5, BABYLON.Vector3.Zero(), scene
+            "Camera", -(Math.PI / 2) + 1, Math.PI / 2, 5, BABYLON.Vector3.Zero(), scene
+        );
+
         camera.attachControl(canvas, true);
-        // camera.inputs.attached.mousewheel.detachControl(canvas);
-
-        // Set up zoom functionality
-        const zoomSpeed = 0.1;
-        const zoomIn = (delta) => {
-            camera.position.z -= delta * zoomSpeed;
-        };
-        const zoomOut = (delta) => {
-            camera.position.z += delta * zoomSpeed;
-        };
-
-        // Add event listeners for zooming
-        window.addEventListener('keydown', (event) => {
-            if (event.key === 'ArrowUp') {
-                zoomIn(event.deltaY);
-            } else if (event.key === 'ArrowDown') {
-                zoomOut(event.deltaY);
-            }
-        });
 
         var dome = new BABYLON.PhotoDome(
             "",
             "/models/studio.jpg",
             {
                 resolution: 32,
-                size: 1000
+                size: 1000,
+                useDirectMapping: false
             },
             scene
         );
 
         dome.imageMode = BABYLON.PhotoDome.MODE_SIDEBYSIDE;
 
+
+
+
+
         return scene;
     };
+
+ 
 
     // to rotate ourself
     // scene.beforeRender = function () {
